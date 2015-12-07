42497cbc3761b83cbc24e45136209928e35f463d
Todd Austin <austin.todd.j@gmail.com>
2015-12-07 14:20:06 -0600

    Re-structured project to fix autoloading

--- example.php
+++ example.php
@@ -1,74 +1,111 @@
 <html>
-<head>
-    <title>PHP Zip Code Range and Distance Calculation Class for PostgreSQL</title>
-</head>
-<body>
 
-<h3>More Information</h3>
-<ul>
-    <li><a href="https://github.com/austintoddj/PHP-PostgreSQL-ZipCode-Class">PHP-PostgreSQL-ZipCode-Class</a> source code and downloads on GitHub.</li>
-</ul>
+    <head>
 
-<?php
-// Include the Zip Code Class
-include('ZipCode.php');
+        <title>PHP PostgreSQL ZipCode Class</title>
 
-// Connect to the pgSQL database with the zip code table
-$connectionString = "host=HOST port=PORT dbname=DBNAME user=USER password=PASSWORD";
-$dbConnection     = pg_connect($connectionString);
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-ZipCode::setConnection($dbConnection);
+    </head>
 
-// You can instantiate ZipCodesInCity with a zip code or with city and state
-$brooklyn_park = new ZipCode("55445");
-$minneapolis   = new ZipCode("Minneapolis, MN");
+    <body>
 
-/*
-You can get the distance to another location by specifying a zip code,
-city/state string, or another ZipCodesInCity object. You can specify whether you want
-to get the distance in miles or kilometers.
-*/
-echo "<h2>Get the distance between 2 zip codes</h2>";
+        <div class="container">
 
-$distance1 = round($brooklyn_park->getDistanceTo("55404"), 2);
-$distance2 = round($brooklyn_park->getDistanceTo("Minneapolis, MN"), 2);
+        <?php
 
-echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance1</strong> miles away from "
-     . "zip code <strong>55404</strong><br/>";
-echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance2</strong> miles away from "
-     . "the city <strong>Minneapolis, MN</strong><br/>";
-echo '<hr>';
+        include('./vendor/autoload.php');
 
-$distance1 = round($minneapolis->getDistanceTo("55109"), 2);
-$distance2 = round($minneapolis->getDistanceTo("Maplewood, MN"), 2);
+        use ZipCode\Maps;
 
-echo "Zip code <strong>$minneapolis</strong> is <strong>$distance1</strong> miles away from "
-     . "zip code <strong>55109</strong><br/>";
-echo "Zip code <strong>$minneapolis</strong> is <strong>$distance2</strong> miles away from "
-     . "the city <strong>Maplewood, MN</strong><br/>";
-echo '<hr>';
+        /*
+         * Connect to the pgSQL database with the zip_code table
+         */
+        $connectionString = "host=localhost port=5432 dbname=database user=username password=password";
+        $dbConnection     = pg_connect($connectionString);
 
-/*
-You can get all of the zip codes within a distance range from the zip. Here we
-are doing all zip codes between 0 and 2 miles. The returned array contains the
-distance as the array's key and the array element is another ZipCodesInCity object.
-*/
-echo "<h2>Get all zip codes within 15 miles from 55445 (<i>" . count($brooklyn_park->getZipsInRange(0, 15)) . "</i>)</h2>";
-foreach ($brooklyn_park->getZipsInRange(0, 15) as $miles => $zip) {
-    $miles = round($miles, 1);
-    echo "Zip code <strong>$zip</strong> is <strong>$miles</strong> miles away from "
-         . " <strong>$brooklyn_park</strong> ({$zip->getCounty()} county)<br/>";
-}
-echo '<hr>';
+        Maps::setConnection($dbConnection);
 
-/*
-You can get all of the zip codes within a city.
-*/
-echo "<h2>Get all zip codes in Brooklyn Park, MN</h2>";
-$port = new ZipCode('Brooklyn Park, MN');
-foreach ($port->getZipsInCity() as $value) {
-    var_dump($value);
-}
-?>
-</body>
+        /*
+         * Instantiate ZipCodesInCity with a zip code or with city and state
+         */
+        $brooklyn_park = new Maps("55445");
+        $minneapolis   = new Maps("Minneapolis, MN");
+
+        /*
+         * You can get the distance to another location by specifying a zip code,
+         * city/state string, or another ZipCodesInCity object. You can specify whether you want
+         * to get the distance in miles or kilometers.
+         */
+        $distance1 = round($brooklyn_park->getDistanceTo("55404"), 2);
+        $distance2 = round($brooklyn_park->getDistanceTo("Minneapolis, MN"), 2);
+        $distance3 = round($minneapolis->getDistanceTo("55109"), 2);
+        $distance4 = round($minneapolis->getDistanceTo("Maplewood, MN"), 2);
+
+        ?>
+
+        <h2>Distance Between Zip Codes</h2>
+
+        <hr>
+
+        <h4>With Numeric Zip Codes Supplied</h4>
+
+        <ul>
+
+            <li><?php echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance1</strong> miles away from " . "zip code <strong>55404</strong><br/>"; ?></li>
+
+            <li><?php echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance2</strong> miles away from " . "the city <strong>Minneapolis, MN</strong><br/>"; ?></li>
+
+        </ul>
+
+        <h4>With City Name Supplied</h4>
+
+        <ul>
+
+            <li><?php echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance3</strong> miles away from " . "zip code <strong>55404</strong><br/>"; ?></li>
+
+            <li><?php echo "Zip code <strong>$brooklyn_park</strong> is <strong>$distance4</strong> miles away from " . "the city <strong>Minneapolis, MN</strong><br/>"; ?></li>
+
+        </ul>
+
+        <hr>
+
+        <?php
+
+        /*
+         * You can get all of the zip codes within a distance range from the zip. Here we
+         * 8are doing all zip codes between 0 and 2 miles. The returned array contains the
+         * distance as the array's key and the array element is another ZipCodesInCity object.
+         */
+        echo "<h2>Get all zip codes within 15 miles from 55445 (<i>" . count($brooklyn_park->getZipsInRange(0, 15)) . "</i>)</h2>";
+
+        foreach ($brooklyn_park->getZipsInRange(0, 15) as $miles => $zip) {
+
+            $miles = round($miles, 1);
+
+            echo "Zip code <strong>$zip</strong> is <strong>$miles</strong> miles away from " . " <strong>$brooklyn_park</strong> ({$zip->getCounty()} county)<br/>";
+
+        }
+
+        echo '<hr>';
+
+        /*
+         * You can get all of the zip codes within a city.
+         */
+        echo "<h2>Get all zip codes in Brooklyn Park, MN</h2>";
+
+        $brooklyn_park = new Maps('Brooklyn Park, MN');
+
+        foreach ($brooklyn_park->getZipsInCity() as $value) {
+
+            var_dump($value);
+
+        }
+
+        ?>
+
+        </div>
+
+    </body>
+
 </html>
---
+++ src/ZipCode/Maps.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ZipCode;
+
+class Maps extends ZipCode {
+
+    /**
+     * @var resource PostgreSQL connection resource
+     */
+    protected static $dbConnection = null;
+
+    /**
+     * Constructor.  Must call static::setConnection before constructor.
+     *
+     * @param string $location zip code or city, state combination
+     *
+     * @throws \UnexpectedValueException
+     */
+    public function __construct($location) {
+        parent::__construct($location);
+        $this->pg_table = 'maps';
+        if (is_null(static::$dbConnection)) {
+            throw new \UnexpectedValueException('Expected database connection resource but got null.');
+        }
+        $this->pg_conn = static::$dbConnection;
+    }
+
+    /**
+     * Get PostgreSQL connection resource.
+     *
+     * @return resource
+     */
+    public static function getConnection() {
+        return static::$dbConnection;
+    }
+
+    /**
+     * Set PostgreSQL connection resource.  This is the first method that must be called before the class is used.
+     *
+     * @param resource $dbConnection
+     */
+    public static function setConnection($dbConnection) {
+        static::$dbConnection = $dbConnection;
+    }
+
+    /**
+     *  Class ZipCodesInCity
+     *
+     *  This class provides a list of zip codes within a city if a city and state_prefix are given.
+     *
+     * @return  array
+     */
+    public function getZipsInCity() {
+
+        $sql  = "SELECT * FROM {$this->pg_table} t "
+                ."WHERE city = '{$this->city}' "
+                ."AND state_prefix = '{$this->state_prefix}' ";
+
+        $result = pg_query($this->pg_conn, $sql);
+
+        return pg_fetch_all($result);
+    }
+
+}
